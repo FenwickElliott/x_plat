287098af42cd09f70492daf720f7f9cf408686a6
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-26 00:46:15 -0500

    Appdir

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
-package main
+package xplat
 
-import "fmt"
+import (
+	"errors"
+	"os"
+	"path"
+	"runtime"
+)
 
-func main() {
-	fmt.Println("x_platform")
+func Appdir() (string, error) {
+	var appdir string
+	switch runtime.GOOS {
+	case "darwin":
+		appdir = path.Join(os.Getenv("HOME"), "Library", "Application Support")
+	case "linux":
+		appdir = path.Join(os.Getenv("HOME"), ".config")
+	case "windows":
+		appdir = os.Getenv("APPDATA")
+	default:
+		return "", errors.New("Unrecogniszed os: " + runtime.GOOS)
+	}
+	return appdir, nil
 }
